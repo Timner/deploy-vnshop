fc316dedcedef3ddc9a6b4d07b5bcbdc0e2d0c3e
Timner <1593093589@qq.com>
2017-11-03 14:56:38 +0800

    :vnshop10.sh

--- projects/vnshop10.sh
+++ projects/vnshop10.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
  
-WEB_PATH='/home/wwwroot/vnshop/'
-WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
+WEB_PATH='/home/wwwroot/vnshopx/'
+WEB_PATH_CLIENT='/home/wwwroot/vnshopx/client'
 WEB_USER='www'
 WEB_USERGROUP='www'
 # we can do 
-echo "Start deployment vx.itnote.cn"
+echo "Start deployment aa.pengx.top"
 cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/release
